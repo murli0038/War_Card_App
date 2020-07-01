4ca81b426c1f5250f810fb824e8bc5a1a75c7ef2
Nikunj Prajapati <prajapatimurli2000@gmail.com>
2020-07-01 17:32:31 +0530

    done

--- myProject/ViewController.swift
+++ myProject/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    //outlets
+    @IBOutlet weak var leftImage: UIImageView!
+    @IBOutlet weak var rightImage: UIImageView!
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var systemScore: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func dealButtonTapped(_ sender: UIButton)
+    {
+        let leftNum = Int.random(in: 2...14)
+        let rightNum = Int.random(in: 2...14)
+        
+        leftImage.image = UIImage(named: "card\(leftNum)")
+        rightImage.image = UIImage(named: "card\(rightNum)")
+        
+        if leftNum > rightNum
+        {
+            leftScore += 1
+            playerScore.text = String(leftScore)
+        }
+        else if leftNum < rightNum
+        {
+            rightScore += 1
+            systemScore.text = String(rightScore)
+        }
+        else
+        {
+            
+        }
+    }
+    
 }
 
